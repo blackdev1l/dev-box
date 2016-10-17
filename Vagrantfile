296d9e76321a559000c0d9b7a963b55635f24f4b
Cristian <blackdevil93@gmail.com>
2016-10-17 23:04:58 +0200

    add i3-gaps to vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -4,11 +4,18 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/wily64"
   config.vm.hostname = 'dev-box'
+  config.vm.synced_folder '.', '/vagrant', disabled: true
 
   config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true, privileged: false
 
-  config.vm.provider 'virtualbox' do |v|
-    v.memory = 8192
-    v.cpus = 2
+  config.vm.provider 'virtualbox' do |vb|
+    vb.gui = true
+    vb.cpus = 2
+    vb.customize ["modifyvm", :id, "--name", "dev-box"]
+    vb.customize ["modifyvm", :id, "--memory", "4096"]
+    vb.customize ["modifyvm", :id, "--vram", 64]
+    vb.customize ["modifyvm", :id, "--accelerate3d", "on"]
+    vb.customize ['modifyvm', :id, '--clipboard', 'bidirectional']
+    vb.customize ['modifyvm', :id, '--draganddrop', 'bidirectional']
   end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,13 @@ function install {
     sudo apt-get -y install "$@" >/dev/null 2>&1
 }
 
-echo updating package information
-apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
-apt-get -y update >/dev/null 2>&1
+apt-get update > /dev/null 2>&1
+apt-get upgrade > /dev/null 2>&1
 
 install 'development tools' build-essential
+install Urxvt rxvt-unicode-256color
+install Rofi rofi
+install Python python python-pillow
 install Git git
 install vim vim-nox
 install Zsh zsh
@@ -16,8 +18,27 @@ install Wget wget
 install Tmux tmux
 install Weechat weechat
 install Golang golang
+intall Fonts fonts-font-awesome fonts-inconsolata
+install 'Vbox guest additions' virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
-#install 'i3-gaps dependencies' libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
+install 'i3-gaps dependencies' xorg fonts-font-awesome libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
+
+# run GUI as non-privileged user
+sudo echo 'allowed_users=anybody' > /etc/X11/Xwrapper.config
+
+echo 'Installing i3-gaps and i3-blocks'
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+make
+sudo make install
+cd ..
+git clone git://github.com/vivien/i3blocks i3blocks
+cd i3blocks
+sudo make install
+cd ~
+rm -r i3-gaps i3blocks
+echo 'done!'
 
 echo 'installing vundle for vim'
 mkdir -p ~/.vim
@@ -25,6 +46,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo 'downloading dotfiles'
 git clone https://github.com/blackdev1l/dotfiles ~/dotfiles
+cp -R ~/dotfiles/.config ~/.config
 cp -R ~/dotfiles/.config ~/.config
 cp -R ~/dotfiles/.weechat ~/.weechat
 cd ~/dotfiles
@@ -39,6 +61,7 @@ echo 'setting up golang workspace'
 mkdir -p /dev/go
 echo export GOPATH=~/dev/go >> ~/.zshrc
 echo export PATH=$GOPATH/bin:$PATH >> ~/.zshrc
+echo exec i3 >> .xinitrc
 
 echo 'all set, rock on!'
 echo 'remember to chsh -s /bin/zsh'
